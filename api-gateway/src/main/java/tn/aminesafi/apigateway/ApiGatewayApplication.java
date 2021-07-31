fba781ac7609c30f0b3bf6bc93fb46ee7eb41eb7
SAFI <aminesafig@gmail.com>
2021-07-31 17:35:01 +0100

    set instance properties

--- api-gateway/src/main/java/tn/aminesafi/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/tn/aminesafi/apigateway/ApiGatewayApplication.java
@@ -3,7 +3,6 @@ package tn.aminesafi.apigateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableDiscoveryClient
 @SpringBootApplication
